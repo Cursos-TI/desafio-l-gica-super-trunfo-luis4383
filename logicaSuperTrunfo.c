0091959ead3d4f6a78401bfe8a99a8b58078add8
luis4383 <luisapbc@hotmail.com>
2025-05-20 01:10:28 +0000

    pronto

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,134 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+
+    typedef struct {
+    char nome[50];
+    unsigned long int populacao;
+    float area;
+    float pib;
+    int num_pontos_turisticos;
+    float densidade_demografica;
+    } Carta;
+
+
+    float calcular_densidade(unsigned long int populacao, float area) {
+    if (area > 0) {
+        return (float)populacao / area;
+    } else {
+       
+    }
+}
+
+    int main() {
+    Carta carta1, carta2;
+    int escolha;
+
+   
+    strcpy(carta1.nome, "distrito federal");
+    carta1.populacao = 213000;
+    carta1.area = 8516.0;
+    carta1.pib = 14400000.0;
+    carta1.num_pontos_turisticos = 15;
+    carta1.densidade_demografica = calcular_densidade(carta1.populacao, carta1.area);
 
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    strcpy(carta2.nome, "bahia");
+    carta2.populacao = 450000;
+    carta2.area = 2780025.0;
+    carta2.pib = 4900000.0;
+    carta2.num_pontos_turisticos = 25;
+    carta2.densidade_demografica = calcular_densidade(carta2.populacao, carta2.area);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("--- Super Trunfo de estados ---\n\n");
+    printf("Carta 1: %s\n", carta1.nome);
+    printf("Carta 2: %s\n\n", carta2.nome);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+     {
+    printf("Escolha o atributo para comparar:\n");
+    printf("1 - Populacao\n");
+    printf("2 - Area\n");
+    printf("3 - PIB\n");
+    printf("4 - Numero de Pontos Turisticos\n");
+    printf("5 - Densidade Demografica\n");
+    printf("0 - Sair\n");
+    printf("Sua escolha: ");
+    scanf("%d", &escolha);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+printf("\n--- Resultado da Comparacao ---\n");
+printf("estados: %s vs %s\n", carta1.nome, carta2.nome);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+switch (escolha) {
+ case 1:
+printf("Atributo: Populacao\n");
+printf("%s: %lu\n", carta1.nome, carta1.populacao);
+printf("%s: %lu\n", carta2.nome, carta2.populacao);
+if (carta1.populacao > carta2.populacao) {
+printf("Vencedor: %s\n", carta1.nome);
+} else if (carta2.populacao > carta1.populacao) {
+printf("Vencedor: %s\n", carta2.nome);}
+ else {
+printf("Empate!\n");}
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+break;
+ case 2:
+printf("Atributo: Area\n");
+printf("%s: %.2f\n", carta1.nome, carta1.area);
+printf("%s: %.2f\n", carta2.nome, carta2.area);
+if (carta1.area > carta2.area) {
+printf("Vencedor: %s\n", carta1.nome);}
+ else if (carta2.area > carta1.area) {
+printf("Vencedor: %s\n", carta2.nome);}
+ else {
+printf("Empate!\n");}
+
+break;
+case 3:
+printf("Atributo: PIB\n");
+printf("%s: %.2f\n", carta1.nome, carta1.pib);
+printf("%s: %.2f\n", carta2.nome, carta2.pib);
+if (carta1.pib > carta2.pib) {
+printf("Vencedor: %s\n", carta1.nome);}
+ else if (carta2.pib > carta1.pib) {
+printf("Vencedor: %s\n", carta2.nome);}
+else {       
+  printf("Empate!\n");}
+
+break;
+case 4:
+printf("Atributo: Numero de Pontos Turisticos\n");
+printf("%s: %d\n", carta1.nome, carta1.num_pontos_turisticos);
+printf("%s: %d\n", carta2.nome, carta2.num_pontos_turisticos);
+if (carta1.num_pontos_turisticos > carta2.num_pontos_turisticos) {
+printf("Vencedor: %s\n", carta1.nome);}
+ else if (carta2.num_pontos_turisticos > carta1.num_pontos_turisticos) {
+printf("Vencedor: %s\n", carta2.nome);}
+ else {
+printf("Empate!\n");}
+
+break;
+case 5:
+printf("Atributo: Densidade Demografica\n");
+printf("%s: %.2f\n", carta1.nome, carta1.densidade_demografica);
+printf("%s: %.2f\n", carta2.nome, carta2.densidade_demografica);
+if (carta1.densidade_demografica < carta2.densidade_demografica) {
+printf("Vencedor: %s\n", carta1.nome);
+} else if (carta2.densidade_demografica < carta1.densidade_demografica) {
+printf("Vencedor: %s\n", carta2.nome);}
+ else {
+printf("Empate!\n");}
+
+break;
+case 0:
+printf("Saindo do jogo.\n");
+break;
+default:
+printf("Opcao invalida. Tente novamente.\n");}
+
+printf("\n");}
+    
 
     return 0;
-}
+
+
+ }
